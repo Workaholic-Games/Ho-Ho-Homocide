e5985e72721ad8d8ec2eefeb74a93eb139b1fba0
NinjaBot2010 <jonathan.emmons@outlook.com>
2026-06-29 20:34:15 +0200

    lots o things

---
+++ follow_cam.gd
@@ -0,0 +1,10 @@
+extends Camera2D
+
+@export var tilemap : TileMapLayer
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var mapsize = tilemap.get_used_rect()
+	var tilesize = tilemap.rendering_quadrant_size
+	var worldsizepixels = mapsize.size * tilesize
+	limit_right = worldsizepixels.x
+	limit_bottom = worldsizepixels.y
