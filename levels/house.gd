7b32adf5e66d894ef7999d940102f2e1706ff080
NinjaBot2010 <jonathan.emmons@outlook.com>
2026-07-01 07:43:44 +0200

    Working on level data

--- levels/house.gd
+++ levels/house.gd
@@ -1,4 +1,24 @@
 extends Node2D
 
-func _process(_delta: float) -> void:
-	$CanvasLayer/ProgressBar.value = $"Awake Timer".wait_time - $"Awake Timer".time_left
+var level = LevelData.new()
+var hazards = level.hazards
+
+var duration : float = 300.0
+@onready var awake_timer: ColorRect = $CanvasLayer/AwakeTime
+
+func _ready() -> void:
+	start_awake_timer()
+
+func start_awake_timer() -> void:
+	var shader_mat = awake_timer.material as ShaderMaterial
+	shader_mat.set_shader_parameter("progress", 0.0)
+	
+	var tween = create_tween()
+	tween.tween_property(shader_mat, "shader_parameter/progress", 1.0, duration)
+	tween.tween_callback(end)
+
+func end():
+	print("[Member] has woken up!")
+
+func set_hazards():
+	pass
--- scripts/LevelData.gd
+++ scripts/LevelData.gd
@@ -1,13 +1,21 @@
 extends Resource
 class_name  LevelData
 
-var difficulty : int
+var difficulty : int = 1
 
-var hazards : Array[String] = [
-	"Floorboard",
-	"Can",
-	"Toy",
-]
+var hazards : Dictionary = {
+	"Floorboard": {
+		"noise_level": 3,
+	},
+	"Can": {
+		"noise_level": 6
+	},
+	"Toy": {
+		"noise_level": 10
+	}
+}
+
+var active_hazards : Dictionary = {}
 
 var naughty_clues : Array[String] = [
 	"Dirt",
@@ -17,3 +25,13 @@ var nice_clue : Array[String] = [
 	"Milk and Cookies",
 	"Clean",
 ]
+
+func scale_hazards() -> void:
+	active_hazards.clear()
+	var multiplier = 1.0 + ((difficulty-1) * 0.2)
+	for hazard_name in hazards:
+		var noise = hazards[hazard_name]["noise_level"]
+		var scaled_noise = roundi(noise * multiplier)
+		active_hazards[hazard_name] = {
+			"noise_level": scaled_noise
+		}
